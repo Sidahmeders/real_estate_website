879270a2162235ba85f5658625a3c069df4ad67c
sidahmed boutaraa <ahorob57@gmail.com>
2020-04-17 21:46:25 +0200

    the killing

--- client/src/components/about.component.js
+++ client/src/components/about.component.js
@@ -6,12 +6,14 @@ function About() {
 
   const context = useContext(ContextConsumer);
   const usersInfo = context.houses.usersInfo;
+  console.log(usersInfo)
   
     return(
        <div className="about-page">
          <h1>THE ABOUT PAGE</h1>
          <h2>About this page is some hidden stuff</h2>
          {usersInfo ? usersInfo.map(house => {
+
            return (
              <div key={house._id}>
                <h2>{house.address}</h2>
