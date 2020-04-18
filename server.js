d1dcbe933603873dbc7bf5f5d55b4f4253236a00
sidahmed boutaraa <ahorob57@gmail.com>
2020-04-18 04:57:44 +0200

    the same problems

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use('/userLocation', require('./routes/userLoc.routes'));
 app.use('/users', require('./routes/users.routes'));
 
 
-if(process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV !== 'development') {
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
